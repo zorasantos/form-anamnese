46b61119fcf6447f834c221a780e6842b394505e
zorasantos <zoranildo22@gmail.com>
2023-04-07 19:09:10 -0300

    feat: first commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import App from "./App.vue";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { router } from "./router";
+
+import "./style.css";
+
+const pinia = createPinia();
+
+createApp(App).use(router).use(pinia).mount("#app");
